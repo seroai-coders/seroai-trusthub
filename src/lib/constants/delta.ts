c07f603e344bccaacf04aeb28d9f0fe9c297501f
Shivani <shivani@nicatch.com>
2024-02-16 18:50:55 -0400

    Format

--- src/lib/constants/delta.ts
+++ src/lib/constants/delta.ts
@@ -1,6 +1,6 @@
 export const findDelta = <T extends { id: string }>(
   arr1: T[] = [],
-  arr2: T[] = [],
+  arr2: T[] = []
 ) => ({
   create: arr2.filter((i) => !arr1.find(({ id }) => id === i.id)),
   delete: arr1.filter((i) => !arr2.find(({ id }) => id === i.id)),
